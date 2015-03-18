9449f3b8aa9ffdb5d6e1287b8c549e90a047fa21
James Cox <james@imaj.es>
2015-03-18 17:13:06 -0400

    Projects request spec, rescue 404

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,8 @@
 class ApiController < ApplicationController
   respond_to :json
+
+  rescue_from ActiveRecord::RecordNotFound do
+    respond_with [], status: :not_found
+  end
+
 end
--- spec/requests/api/projects_spec.rb
+++ spec/requests/api/projects_spec.rb
@@ -14,38 +14,115 @@ RSpec.describe 'API :: Projects', type: :request do
   end
 
   context 'GET #index' do
-    before do
-      get_json '/api/projects'
-    end
+    context 'no filter' do
+      before do
+        get_json '/api/projects'
+      end
 
-    it 'should have a valid status' do
-      expect(response).to be_success
-    end
+      it 'should have a valid status' do
+        expect(response).to be_success
+      end
 
-    it 'responds with all the projects' do
-      expect(parsed_body.keys.first).to eq('projects')
-    end
+      it 'responds with all the projects' do
+        expect(parsed_body.keys.first).to eq('projects')
+      end
 
-    it 'should have the correct keys' do
-      expect(parsed_body['projects'].first.keys).to match_array(['category', 'city_ref', 'id', 'permits_count', 'title'])
+      it 'should have the correct keys' do
+        expect(parsed_body['projects'].first.keys).to match_array(['category', 'city_ref', 'id', 'permits_count', 'title'])
+      end
     end
   end
 
-  context 'GET #index with category name' do
-    before do
-      get_json '/api/projects', category_name: @project.category.name
-    end
+  context 'filter by category' do
+    context 'GET #index with real category id' do
+      before do
+        get_json '/api/projects', category_id: @project.category.id
+      end
 
-    it 'should have a valid status' do
-      expect(response).to be_success
-    end
+      it 'should have a valid status' do
+        expect(response).to be_success
+      end
 
-    it 'responds with all the projects' do
-      expect(parsed_body.keys.first).to eq('projects')
+      it 'responds with all the projects' do
+        expect(parsed_body.keys.first).to eq('projects')
+      end
     end
+    context 'GET #index with invalid category id' do
+      before do
+        get_json '/api/projects', category_id: 0
+      end
 
-    it 'should have the correct keys' do
-      expect(parsed_body['projects'].first.keys).to match_array(['category', 'city_ref', 'id', 'permits_count', 'title'])
+      it 'should have a valid status' do
+        expect(response).to be_success
+      end
+
+      it 'should have no results' do
+        expect(parsed_body['projects'].size).to eq(0)
+      end
     end
   end
+
+  context 'filtered by company' do
+    context 'GET #index with company id' do
+      before do
+        get_json '/api/projects', company_id: @company.id
+      end
+
+      it 'should have a valid status' do
+        expect(response).to be_success
+      end
+
+      it 'responds with all the projects' do
+        expect(parsed_body.keys.first).to eq('projects')
+      end
+    end
+    context 'GET #index with invalid company id' do
+      before do
+        get_json '/api/projects', company_id: 0
+      end
+
+      it 'should have a valid status' do
+        expect(response).to be_success
+      end
+
+      it 'should have no results' do
+        expect(parsed_body['projects'].size).to eq(0)
+      end
+    end
+  end
+
+  context 'GET #show' do
+    context 'existing project' do
+      before do
+        get_json "/api/projects/#{@project.id}"
+      end
+
+      it 'should have a valid status' do
+        expect(response).to be_success
+      end
+
+      it 'responds with all the projects' do
+        expect(parsed_body.keys.first).to eq('project')
+      end
+
+      it 'should have the correct keys' do
+        expect(parsed_body['project'].keys).to match_array(['category', 'city_ref', 'id', 'permits_count', 'title'])
+      end
+    end
+
+    context 'invalid project id' do
+      before do
+        get_json '/api/projects/0'
+      end
+
+      it 'should have a valid status' do
+        expect(response).to be_not_found
+      end
+
+      it 'responds with all the projects' do
+        expect(parsed_body.keys.first).to eq('projects')
+      end
+    end
+  end
+
 end
