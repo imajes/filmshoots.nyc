8717de690125949368ab9253aebeef8cfe767f77
James Cox <james@imaj.es>
2015-03-26 12:46:18 -0400

    Set configs for heroku

--- config/puma.rb
+++ config/puma.rb
@@ -1,21 +1,16 @@
-workers ENV.fetch('PUMA_WORKERS', 3).to_i
-threads ENV.fetch('MIN_THREADS', 1).to_i, ENV.fetch('MAX_THREADS', 10).to_i
+workers Integer(ENV.fetch('WEB_CONCURRENCY', 2))
+threads_count = Integer(ENV.fetch('MAX_THREADS', 5))
+threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
-environment ENV.fetch('RAILS_ENV', 'development')
-bind        ENV.fetch('PUMA_BIND_PATH', 'tcp://0.0.0.0:3000')
-
-activate_control_app 'tcp://0.0.0.0:9292', { no_token: true }
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # worker specific setup
-  ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env]
-    config['pool'] = ENV.fetch('DB_POOL_SIZE', 16)
-    ActiveRecord::Base.establish_connection(config)
-  end
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
 end
-
 
